cdd435bd8df86b2a2c52ae8df08f16d962bdc0af
Dudermont <dkprogramer@gmail.com>
2023-01-16 00:51:07 +0300

    15.02

---
+++ 205. Isomorphic Strings.py
@@ -0,0 +1,12 @@
+def isIsomorphic(s, t):
+    st, ts = {}, {}
+    for c1, c2 in zip(s, t):
+        if ((c1 in st and st[c1] != c2) or
+                (c2 in ts and ts[c2] != c1)):
+            return False
+        st[c1] = c2
+        ts[c2] = c1
+    return True
+
+
+print(isIsomorphic('bbbaaaba', 'aaabbbba'))
